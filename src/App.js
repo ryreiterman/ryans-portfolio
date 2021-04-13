855ff5a6cfa9de4ba54793c39404567010384c08
Ryan Reiterman <ryreiterman@gmail.com>
2021-04-12 23:42:16 -0400

    Added more projects, more styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import Project from './components/Project'
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Project />
-      </header>
-    </div>
+    <>
+      <div className="header-container">
+        <h1 className="header-h1">Ryan Reiterman</h1>
+        <ul className="header-contact">
+          <li>
+            <a href="mailto:ryreiterman@gmail.com">ryreiterman@gmail.com</a>
+          </li>{" "}
+          |{" "}
+          <li>
+            <a target="new" href="https://www.linkedin.com/in/ryanreiterman/">
+              linkedin.com/in/ryanreiterman
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <Project />
+        </header>
+      </div>
+    </>
   );
 }
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,11 @@ import React, { useState, useEffect } from "react";
 
 const Projects = () => {
 
-    const projects =[{}]
+    const projects =[
+        {
+
+    }
+]
 
     return (
       <>
@@ -11,20 +15,35 @@ const Projects = () => {
           <div className="project-row">
             <div className="project-image">
               <img
-                src="https://res.cloudinary.com/ryanphotos/image/upload/v1618273708/demos/final_project_screenshot_600px_ljus0v.jpg"
+                src="https://res.cloudinary.com/ryanphotos/image/upload/v1618279302/demos/sesame_memory_game_screenshot_600px_wleftr.jpg"
                 alt="Project screenshot"
               />
             </div>
             <div className="project-text">
-              <h3>Project 1</h3>
-              <p>Description: A game that is awesome</p>
+              <h3>Sesame Street Memory Game</h3>
               <p>
-                Link:{" "}
+                Compete against a friend to see who can match the most cards
+                before times runs out!
+              </p>
+              <p>
+                Github Repo
+                <br />
                 <a
                   target="new"
-                  href="https://final-project-photo-gallery.herokuapp.com/"
+                  href="https://github.com/ryreiterman/sesame-street-memory-game"
                 >
-                  final-project-photo-gallery.herokuapp.com
+                  github.com/ryreiterman/sesame-street-memory-game
+                </a>
+              </p>
+
+              <p>
+                Project Link
+                <br />
+                <a
+                  target="new"
+                  href="https://ryreiterman.github.io/sesame-street-memory-game/"
+                >
+                  ryreiterman.github.io/sesame-street-memory-game
                 </a>
               </p>
             </div>
@@ -33,15 +52,101 @@ const Projects = () => {
           <div className="project-row">
             <div className="project-image">
               <img
+                src="https://res.cloudinary.com/ryanphotos/image/upload/v1618279302/demos/movies_app_screenshot_600px_zwtaqg.jpg"
+                alt="Project screenshot"
+              />
+            </div>
+            <div className="project-text">
+              <h3>Movie Search App</h3>
+              <p>
+                Search a movie database and create a list of your favorite
+                movies to share with friends.
+              </p>
+              <p>
+                Github Repo
+                <br />
+                <a
+                  target="new"
+                  href="https://github.com/ryreiterman/ryans-movie-search-app"
+                >
+                  github.com/ryreiterman/ryans-movie-search-app
+                </a>
+              </p>
+              <p>
+                Project Link
+                <br />
+                <a
+                  target="new"
+                  href="https://ryans-movie-search-app.herokuapp.com/"
+                >
+                  ryans-movie-search-app.herokuapp.com
+                </a>
+              </p>
+            </div>
+          </div>
+
+          <div className="project-row">
+            <div className="project-image">
+              <img
+                src="https://res.cloudinary.com/ryanphotos/image/upload/v1618279302/demos/jeopardy_screenshot_600px_tg8wyr.jpg"
+                alt="Project screenshot"
+              />
+            </div>
+            <div className="project-text">
+              <h3>Jeopardy App</h3>
+              <p>
+                Pick a random question and see how many you can get right. Add a
+                point for each correct answer, subtract one for every incorrect
+                guess.
+              </p>
+              <p>
+                Github Repo
+                <br />
+                <a
+                  target="new"
+                  href="https://github.com/ryreiterman/react-jeopardy"
+                >
+                  github.com/ryreiterman/react-jeopardy
+                </a>
+              </p>
+              <p>
+                Project Link
+                <br />
+                <a
+                  target="new"
+                  href="https://ryans-jeopardy-app.herokuapp.com/"
+                >
+                  ryans-jeopardy-app.herokuapp.com
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="project-row">
+            <div className="project-image">
+              <img
                 src="https://res.cloudinary.com/ryanphotos/image/upload/v1618273708/demos/final_project_screenshot_600px_ljus0v.jpg"
                 alt="Project screenshot"
               />
             </div>
             <div className="project-text">
-              <h3>Project 2</h3>
-              <p>Description: A game that is awesome</p>
+              <h3>Ryan's Super Cool Photo Galleries</h3>
               <p>
-                Link:{" "}
+                Check out all of my favorite pics from my photography portfolio.
+              </p>
+              <p>
+                Github Repo
+                <br />
+                <a
+                  target="new"
+                  href="https://github.com/ryreiterman/final-project-photo-gallery"
+                >
+                  github.com/ryreiterman/final-project-photo-gallery
+                </a>
+              </p>
+
+              <p>
+                Project Link
+                <br />
                 <a
                   target="new"
                   href="https://final-project-photo-gallery.herokuapp.com/"
